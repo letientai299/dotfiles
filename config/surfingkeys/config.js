1de051266c755cd6667c6fae266980009118ab72
Tai <letientai299@gmail.com>
2022-03-28 16:19:56 +0700

    Continue removing obsolete surfingkeys config

--- config/surfingkeys/config.js
+++ config/surfingkeys/config.js
@@ -1,26 +1,14 @@
 settings.nexstLinkRegex = />|=>|->|>>|\b(next)\b|\b(more)\b|▶|Chap kế/i;
 
-vmapkey("<Ctrl-[>", "Leave visual mode", function () {
-  Visual.visualClear();
-  Visual.exit();
-});
-
-vmapkey("<Shift-Enter>", "Open link in the new tab", function () {
-  RUNTIME("openLink", {
-    tab: { tabbed: true, active: false },
-    url: document.getSelection().focusNode.parentNode.href,
-  });
-});
-
-mapkey("gp", "Toggle pin tab", function () {
+api.mapkey("gp", "Toggle pin tab", function () {
   RUNTIME("togglePinTab");
 });
 
-mapkey("i", "Passthrough mode", function () {
+api.mapkey("i", "Passthrough mode", function () {
   PassThrough.enter();
 });
 
-mapkey(
+api.mapkey(
   "sU",
   "#4Edit current URL with vim editor and open within the current tab",
   function () {
@@ -34,13 +22,13 @@ mapkey(
   }
 );
 
-addSearchAlias("y", "youtube", "https://www.youtube.com/search?q=");
-addSearchAlias(
+api.addSearchAlias("y", "youtube", "https://www.youtube.com/search?q=");
+api.addSearchAlias(
   "t",
   "translate",
   "https://translate.google.com/#view=home&op=translate&sl=auto&tl=vi&text="
 );
 
-mapkey("oy", "#8Open Search with alias y", function () {
+api.mapkey("oy", "#8Open Search with alias y", function () {
   Front.openOmnibar({ type: "SearchEngine", extra: "y" });
 });
