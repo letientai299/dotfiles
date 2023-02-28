9cf0e6bd9acba5840d45388ab36df19262722d8c
Tai <tai.le@shopee.com>
2023-02-28 19:00:00 +0800

    Sync dot 2023-02-28 Tue 19:00:00

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -4,34 +4,34 @@ require('ui')
 -- Tree sitter
 --------------------------------------------------------------------------------
 require 'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    "c", "lua", "rust", "go", "tsx", "dart", "json", "kotlin"
-  },
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "c", "lua", "rust", "go", "tsx", "dart", "json", "kotlin"
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "javascript" },
 
-  highlight = {
-    enable = true,
-    disable = { "rust", "markdown" },
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+        disable = { "rust", "markdown" },
+        additional_vim_regex_highlighting = false,
+    },
 
-  rainbow = {
-    enable = true,
-    -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    extended_mode = true,
-  },
+    rainbow = {
+        enable = true,
+        -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        extended_mode = true,
+    },
 
-  incremental_selection = { enable = true },
-  textobjects = { enable = true },
+    incremental_selection = { enable = true },
+    textobjects = { enable = true },
 }
 
 
