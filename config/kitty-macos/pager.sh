87216f3f8ec13971d1493794fe929244c88dd223
Tai <tai.le@shopee.com>
2023-03-14 11:00:00 +0800

    Sync dot 2023-03-14 Tue 11:00:00

--- config/kitty-macos/pager.sh
+++ config/kitty-macos/pager.sh
@@ -11,25 +11,25 @@ else
   AUTOCMD_TERMCLOSE_CMD="normal G"
 fi
 
-/usr/local/bin/nvim \
+# /usr/local/bin/nvim \
+#   -c "set laststatus=0" \
+#   -c "set colorcolumn=0" \
+#   -c "set tw=10000 nowrap" \
+#   -c "silent CocDisable" \
+#   -c "map <silent> q :qa!<CR>" \
+#   -c "let baleia = luaeval(\"require('baleia').setup { }\")" \
+#   -c "call baleia.once(bufnr('%'))" \
+#   -c "BufferLineGroupToggle ungrouped"
+
+/usr/local/bin/nvim 63<&0 0</dev/null \
   -c "set nonumber" \
   -c "set laststatus=0" \
   -c "set colorcolumn=0" \
   -c "set tw=10000" \
   -c "silent CocDisable" \
+  -c "autocmd TermEnter * stopinsert" \
+  -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
   -c "map <silent> q :qa!<CR>" \
-  -c "BufferLineGroupbaleiaToggle ungrouped" \
-
-
-# /usr/local/bin/nvim 63<&0 0</dev/null \
-#   -c "set nonumber" \
-#   -c "set laststatus=0" \
-#   -c "set colorcolumn=0" \
-#   -c "set tw=10000" \
-#   -c "silent CocDisable" \
-#   -c "autocmd TermEnter * stopinsert" \
-#   -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
-#   -c "map <silent> q :qa!<CR>" \
-#   -c 'terminal sed </dev/fd/63 && sleep 0.01 && printf "\x1b]2;"' \
-#   -c "BufferLineGroupToggle ungrouped"
+  -c 'terminal sed </dev/fd/63 && sleep 0.01 && printf "\x1b]2;"' \
+  -c "BufferLineGroupToggle ungrouped"
 
