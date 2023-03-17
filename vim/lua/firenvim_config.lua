18f679806aab74fc93ab47bc4da00121652b2d88
Tai <letientai299@gmail.com>
2023-03-18 01:00:00 +0700

    Sync dot 2023-03-18 Sat 01:00:00

--- vim/lua/firenvim_config.lua
+++ vim/lua/firenvim_config.lua
@@ -1,23 +1,35 @@
 vim.g.firenvim_config = {
-	globalSettings = { alt = "all" },
-	localSettings = {
-		[".*"] = {
-			cmdline = "neovim",
-			takeover = "never",
-		},
-	},
+  globalSettings = { alt = "all" },
+  localSettings = {
+    [".*"] = {
+      cmdline = "neovim",
+      takeover = "never",
+    },
+  },
 }
 
--- font
+if vim.g.started_by_firenvim == true then
+  vim.o.guifont = "IosevkaTerm Nerd Font Light:h12"
+  vim.api.nvim_create_autocmd({ "UIEnter" }, {
+    callback = function(event)
+      local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
+      if client ~= nil and client.name == "Firenvim" then
+        vim.o.laststatus = 0
+        vim.cmd("silent CocDisable")
+      end
+    end,
+  })
 
-vim.api.nvim_create_autocmd({ "UIEnter" }, {
-	callback = function(event)
-		local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
-		if client ~= nil and client.name == "Firenvim" then
-			vim.o.laststatus = 0
-			vim.o.guifont = "IosevkaTerm Nerd Font Light:h12"
-      vim.cmd('silent CocDisable')
-      vim.cmd('set filetype=markdown')
-		end
-	end,
-})
+  vim.api.nvim_create_autocmd({'TextChanged', 'TextChangedI'}, {
+    callback = function(e)
+      if vim.g.timer_started == true then
+        return
+      end
+      vim.g.timer_started = true
+      vim.fn.timer_start(10000, function()
+        vim.g.timer_started = false
+        write
+      end)
+    end
+  })
+end
