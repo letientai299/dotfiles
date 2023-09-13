fdb3bc82894968d33bc50784cdc43878ba74b3d3
Tai <letientai299@gmail.com>
2023-09-14 02:30:04 +0700

    Sync dot 2023-09-14 Thu 02:30:04 from M2.local

--- p10k.zsh
+++ p10k.zsh
@@ -1380,7 +1380,7 @@ typeset -g POWERLEVEL9K_DIR_HYPERLINK=false
   # POWERLEVEL9K_GCLOUD_PARTIAL_VISUAL_IDENTIFIER_EXPANSION and
   # POWERLEVEL9K_GCLOUD_PARTIAL_CONTENT_EXPANSION to empty.
   typeset -g POWERLEVEL9K_GCLOUD_PARTIAL_CONTENT_EXPANSION='${CLOUDSDK_ACTIVE_CONFIG_NAME:-${P9K_GCLOUD_PROJECT_ID//\%/%%}}'
-  typeset -g POWERLEVEL9K_GCLOUD_COMPLETE_CONTENT_EXPANSION='${P9K_GCLOUD_PROJECT_NAME//\%/%%}'
+  typeset -g POWERLEVEL9K_GCLOUD_COMPLETE_CONTENT_EXPANSION='${CLOUDSDK_ACTIVE_CONFIG_NAME:-${P9K_GCLOUD_PROJECT_NAME//\%/%%}}'
 
   # Send a request to Google (by means of `gcloud projects describe ...`) to obtain project name
   # this often. Negative value disables periodic polling. In this mode project name is retrieved
