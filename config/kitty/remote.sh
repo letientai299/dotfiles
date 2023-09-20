225a768163d5f274cd8ebb8320887f56a0ebfb4f
Tai <letientai299@gmail.com>
2023-09-20 15:30:04 +0700

    Sync dot 2023-09-20 Wed 15:30:04 from M2.local

---
+++ config/kitty/remote.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SOCKET=unix:/tmp/kitty-remote-$RANDOM
+echo $SOCKET
+kitty -o allow_remote_control=yes --listen-on $SOCKET &
+sleep 0.5
+kitty @ --to $SOCKET kitten broadcast
+kitty @ --to $SOCKET new-window
+kitty @ --to $SOCKET new-window
