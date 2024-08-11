c8069664d9fa7e1b456201f64013f05ee8378fb3
Tai <letientai299@gmail.com>
2024-08-11 09:30:04 +0700

    Sync dot 2024-08-11 Sun 09:30:04 from M2.local

--- p10k.zsh
+++ p10k.zsh
@@ -76,7 +76,7 @@
     # phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
     # scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
     # haskell_stack           # haskell version from stack (https://haskellstack.org/)
-    kubecontext             # current kubernetes context (https://kubernetes.io/)
+    # kubecontext             # current kubernetes context (https://kubernetes.io/)
     # terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
     # aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
