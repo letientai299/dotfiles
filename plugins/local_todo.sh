e00d93428a8eaf1fe9cc6be1ec7953352b36abe8
Tai <letientai299@gmail.com>
2025-12-28 07:30:04 +0700

    Sync dot 2025-12-28 Sun 07:30:04 from M2.local

--- plugins/local_todo.sh
+++ plugins/local_todo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 local_todo() {
-  repo=$(git rev-parse --show-toplevel 2>/dev/null)
-  if [[ -z $repo ]]; then
+  git_common_dir=$(git rev-parse --git-common-dir 2>/dev/null)
+  if [[ -z $git_common_dir ]]; then
     echo "not a git repo"
     return
   fi
+
+  # Resolve to the main worktree (parent of .git directory)
+  repo=$(cd "$git_common_dir" && cd .. && pwd)
 
   mkdir -p "$repo"/.dump
   vim "$repo"/.dump/todo.md
