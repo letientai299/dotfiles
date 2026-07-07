f9fbead9849a19bae7e2137e8d9d2bda155f26bc
Tai <letientai299@gmail.com>
2026-07-07 10:50:33 +0700

    Clean up some legacy tools

--- bin/bootstrap-macos.sh
+++ bin/bootstrap-macos.sh
@@ -30,19 +30,6 @@ brew install tmux
 
 task "rg search"
 brew install ripgrep
-wget https://github.com/aykamko/tag/releases/download/v1.4.0/tag_darwin_amd64.zip
-tar -zxf tag_darwin_amd64.zip
-mkdir -p ~/.local/bin
-mv tag ~/.local/bin
-rm tag_darwin_amd64.zip
-
-cat <<HERE >>$HOME/.zshrc_local
-if (( $+commands[tag] )); then
-  export TAG_SEARCH_PROG=rg  # replace with rg for ripgrep
-  tag() { command tag "\$@"; source "\${TAG_ALIAS_FILE:-/tmp/tag_aliases}" 2>/dev/null }
-  alias rg=tag  # replace with rg for ripgrep
-fi
-HERE
 
 task "curl and wget"
 brew install curl wget
@@ -51,4 +38,3 @@ task "Term 256 color"
 wget "https://gist.github.com/sos4nt/3187620/raw/bca247b4f86da6be4f60a69b9b380a11de804d1e/xterm-256color-italic.terminfo"
 tic xterm-256color-italic.terminfo
 rm xterm-256color-italic.terminfo
-
